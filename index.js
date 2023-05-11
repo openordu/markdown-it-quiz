ce31152672b66e9a3326595582432790c62610d2
Chris Godwin <patchshorts@gmail.com>
2023-05-11 15:01:51 -0500

    .

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const container = require('markdown-it-container');
 
 module.exports = function quizPlugin(md) {
+  let currentQuestion = null;
+
   md.use(container, 'quiz', {
     render: function (tokens, idx) {
       if (tokens[idx].nesting === 1) {
         const questionData = tokens[idx].info.trim().split(' ').slice(1).join(' ');
         const correctAnswerIndex = parseInt(questionData.split('|')[0].trim());
         const question = questionData.split('|')[1].trim();
-        return `<div class="question" data-answer="${correctAnswerIndex}"><h2>${question}</h2><div class="list-group">`;
+        currentQuestion = {
+          correctAnswerIndex,
+          question,
+          options: [],
+        };
+        return '';
       } else {
-        return '</div></div>';
+        const { correctAnswerIndex, question, options } = currentQuestion;
+        const questionHtml = `<div class="question" data-answer="${correctAnswerIndex}"><h2>${question}</h2><div class="list-group">${options.join('')}</div></div>`;
+        currentQuestion = null;
+        return questionHtml;
       }
     },
   });
@@ -19,9 +29,34 @@ module.exports = function quizPlugin(md) {
       if (tokens[idx].nesting === 1) {
         return '';
       } else {
-        const optionText = tokens[idx - 1].content.trim();
-        return `<button type="button" class="list-group-item list-group-item-action">${optionText}</button>`;
+        const optionText = tokens[idx - 2].content.trim();
+        const optionHtml = `<button type="button" class="list-group-item list-group-item-action">${optionText}</button>`;
+        if (currentQuestion) {
+          currentQuestion.options.push(optionHtml);
+        }
+        return '';
       }
     },
   });
+
+  md.renderer.rules.text = function (tokens, idx, options, env, self) {
+    if (currentQuestion) {
+      return '';
+    }
+    return self.renderToken(tokens, idx, options);
+  };
+
+  md.renderer.rules.paragraph_open = function (tokens, idx, options, env, self) {
+    if (currentQuestion) {
+      return '';
+    }
+    return self.renderToken(tokens, idx, options);
+  };
+
+  md.renderer.rules.paragraph_close = function (tokens, idx, options, env, self) {
+    if (currentQuestion) {
+      return '';
+    }
+    return self.renderToken(tokens, idx, options);
+  };
 };
